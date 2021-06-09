31bc5e7320eba507aca53144c7ed5a14928020d9
nv <nghiep.voke@gmail.com>
2021-06-09 16:30:34 +0700

    Add README.md file on User Service

---
+++ user-service/README.md
@@ -0,0 +1,7 @@
+#User Service  
+
+The project was used on IntelliJ IDEA tool.
+
+_Note:_   
+For run with command line:  
+> mvn spring-boot:run
--- user-service/src/main/java/com/nghiepvo/userservice/controllers/HelloController.java
+++ user-service/src/main/java/com/nghiepvo/userservice/controllers/HelloController.java
@@ -1,12 +1,12 @@
 package com.nghiepvo.userservice.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String hello() {
         return "This is User Service.";
     }
